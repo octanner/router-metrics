2914e060b165c9e782d227442ae7187b7b6719f5
Murray Resinski <murray.resinski@octanner.com>
2018-10-15 14:18:24 -0600

    COBRA-2114 and COBRA-2133 to gather and store fwd and the tls version.  Also changed debugging and use a single send function to do the delivery.

--- router-metrics.go
+++ router-metrics.go
@@ -15,21 +15,48 @@ import (
 var conn net.Conn
 var err error
 var processed int
-var debugmode bool
+var debugnosend bool
+var debugoutput bool
+var sendfwdtlsversion bool
 
 func main() {
 	processed = 0
-	envdebug := os.Getenv("DEBUG")
-	if envdebug == "" {
-		envdebug = "false"
+	envdebugnosend := os.Getenv("DEBUG_NO_SEND")
+	if envdebugnosend == "" {
+		envdebugnosend = "false"
 	}
-	debugmode, err = strconv.ParseBool(envdebug)
+	debugnosend, err = strconv.ParseBool(envdebugnosend)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("debug mode: %v\n", debugmode)
-	conn, err = net.Dial("tcp", os.Getenv("OPENTSDB_IP"))
+
+	envdebugoutput := os.Getenv("DEBUG_OUTPUT")
+	if envdebugoutput == "" {
+		envdebugoutput = "false"
+	}
+	debugoutput, err = strconv.ParseBool(envdebugoutput)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	envsendfwdtlsversion := os.Getenv("SEND_FWD_TLSVERSION")
+	if envsendfwdtlsversion == "" {
+		envsendfwdtlsversion = "false"
+	}
+	sendfwdtlsversion, err = strconv.ParseBool(envsendfwdtlsversion)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	t := time.Now()
+	consumerindex := t.Format("2006-01-02T15:04:05.999999-07:00")
+	consumergroupnamebase := os.Getenv("CONSUMER_GROUP_NAME")
+	consumergroupname := consumergroupnamebase + "-" + consumerindex
+	fmt.Println(consumergroupname)
+	conn, err = net.Dial("udp", os.Getenv("INFLUX_LINE_IP"))
 	if err != nil {
 		fmt.Println("dial error:", err)
 		os.Exit(1)
@@ -40,7 +67,7 @@ func main() {
 	config.Group.Return.Notifications = true
 	brokers := strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
 	topics := []string{"alamoweblogs"}
-	consumer, err := cluster.NewConsumer(brokers, os.Getenv("CONSUMER_GROUP_NAME"), topics, config)
+	consumer, err := cluster.NewConsumer(brokers, consumergroupname, topics, config)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -57,7 +84,7 @@ func main() {
 		select {
 		case msg, more := <-consumer.Messages():
 			if more {
-				if debugmode {
+				if debugoutput {
 					fmt.Fprintf(os.Stdout, "%s/%d/%d\t%s\t%s\n", msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
 				}
 				sock(string(msg.Value[:]))
@@ -88,125 +115,73 @@ func sock(logline string) {
 	var fieldmap map[string]string
 	fieldmap = make(map[string]string)
 	for _, element := range words {
-		fieldmap[strings.Split(element, "=")[0]] = strings.Split(element, "=")[1]
+		if strings.Contains(element, "=") {
+			fieldmap[strings.Split(element, "=")[0]] = strings.Split(element, "=")[1]
+		}
 	}
 	host := fieldmap["hostname"]
+	var fields string
+	var tags string
 	if !(strings.HasPrefix(host, "alamotest")) && len(words) > 9 && !strings.Contains(strings.Join(words, " "), "4813") {
 		status := fieldmap["status"]
 		service := fieldmap["service"]
 		connect := fieldmap["connect"]
 		total := fieldmap["total"]
 		site_domain := fieldmap["site_domain"]
-		if debugmode {
-			fmt.Println("pushing data for " + host)
+		fwd := "\"" + strings.Split(fieldmap["fwd"], "%")[0] + "\""
+		tlsversion := "\"" + fieldmap["tlsversion"] + "\""
+		tags = "fqdn=" + host
+		if sendfwdtlsversion {
+			fields = "fwd=" + fwd + ",tlsversion=" + tlsversion + ","
 		}
 		metricname := "router.service.ms"
-		timestamp := strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10)
 		value := service
-		put := "put " + metricname + " " + timestamp + " " + value + " fqdn=" + host + "\n"
-		if debugmode {
-			fmt.Println(put)
-		}
-		if !debugmode {
-			fmt.Fprintf(conn, put)
-		}
+		send(metricname, tags, fields+"value="+value)
 
 		metricname = "router.total.ms"
 		value = total
-		put = "put " + metricname + " " + timestamp + " " + value + " fqdn=" + host + "\n"
-		if debugmode {
-			fmt.Println(put)
-		}
-		if !debugmode {
-			fmt.Fprintf(conn, put)
-		}
+		send(metricname, tags, fields+"value="+value)
+
 		metricname = "router.connect.ms"
 		value = connect
-		put = "put " + metricname + " " + timestamp + " " + value + " fqdn=" + host + "\n"
-		if debugmode {
-			fmt.Println(put)
-		}
-		if !debugmode {
-			fmt.Fprintf(conn, put)
-		}
+		send(metricname, tags, fields+"value="+value)
 
 		metricname = "router.status." + status
 		value = "1"
-		put = "put " + metricname + " " + timestamp + " " + value + " fqdn=" + host + "\n"
-		if debugmode {
-			fmt.Println(put)
-		}
-		if !debugmode {
-			fmt.Fprintf(conn, put)
-		}
+		send(metricname, tags, fields+"value="+value)
 
 		metricname = "router.requests.count"
 		value = "1"
-		put = "put " + metricname + " " + timestamp + " " + value + " fqdn=" + host + "\n"
-		if debugmode {
-			fmt.Println(put)
-		}
-		if !debugmode {
-			fmt.Fprintf(conn, put)
-		}
+		send(metricname, tags, fields+"value="+value)
 
 		if site_domain != "" {
 			host = site_domain
-			if debugmode {
-				fmt.Println("pushing data for " + host)
-			}
+			tags = "fqdn=" + host
 			metricname := "router.service.ms"
-			timestamp := strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10)
 			value := service
-			put = "put " + metricname + " " + timestamp + " " + value + " fqdn=" + host + "\n"
-			if debugmode {
-				fmt.Println(put)
-			}
-			if !debugmode {
-				fmt.Fprintf(conn, put)
-			}
+			send(metricname, tags, fields+"value="+value)
 			metricname = "router.total.ms"
 			value = total
-			put = "put " + metricname + " " + timestamp + " " + value + " fqdn=" + host + "\n"
-			if debugmode {
-				fmt.Println(put)
-			}
-			if !debugmode {
-				fmt.Fprintf(conn, put)
-			}
-
+			send(metricname, tags, fields+"value="+value)
 			metricname = "router.connect.ms"
 			value = connect
-			put = "put " + metricname + " " + timestamp + " " + value + " fqdn=" + host + "\n"
-			if debugmode {
-				fmt.Println(put)
-			}
-			if !debugmode {
-				fmt.Fprintf(conn, put)
-			}
-
+			send(metricname, tags, fields+"value="+value)
 			metricname = "router.status." + status
 			value = "1"
-			put = "put " + metricname + " " + timestamp + " " + value + " fqdn=" + host + "\n"
-			if debugmode {
-				fmt.Println(put)
-			}
-			if !debugmode {
-				fmt.Fprintf(conn, put)
-			}
-
+			send(metricname, tags, fields+"value="+value)
 			metricname = "router.requests.count"
 			value = "1"
-			put = "put " + metricname + " " + timestamp + " " + value + " fqdn=" + host + "\n"
-			if debugmode {
-				fmt.Println(put)
-			}
-			if !debugmode {
-				fmt.Fprintf(conn, put)
-			}
+			send(metricname, tags, fields+",value="+value)
 		}
 
 	}
-	processed++
+}
 
+func send(measurement string, tags string, fields string) {
+	if !debugnosend {
+		fmt.Fprintf(conn, measurement+","+tags+" "+fields+"\n")
+	}
+	if debugoutput {
+		fmt.Println(measurement + "," + tags + " " + fields)
+	}
 }

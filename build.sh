615f8378011e61919a873223a6460764a109017c
EC2 Default User <ec2-user@ip-172-31-1-198.us-west-2.compute.internal>
2019-03-26 18:01:01 +0000

    initial full release

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 cd /go/src
 go get "github.com/bsm/sarama-cluster"
+go get "gopkg.in/Shopify/sarama.v1"
 cd /go/src/router-metrics
 go build router-metrics.go
 
--- router-metrics.go
+++ router-metrics.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	cluster "github.com/bsm/sarama-cluster"
 	"log"
@@ -10,7 +11,27 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	//"github.com/Shopify/sarama.v1"
+	"gopkg.in/Shopify/sarama.v1"
 )
+
+type IstioAccessLog struct {
+	Source    string `json:"source"`
+	Method    string `json:"method"`
+	App       string `json:"app"`
+	Bytes     int    `json:"bytes"`
+	Dyno      string `json:"dyno"`
+	Severity  string `json:"severity"`
+	From      string `json:"from"`
+	Space     string `json:"space"`
+	Service   string `json:"service"`
+	Total     string `json:"total"`
+	RequestID string `json:"request_id"`
+	Path      string `json:"path"`
+	Fwd       string `json:"fwd"`
+	Host      string `json:"host"`
+	Status    int    `json:"status"`
+}
 
 var conn net.Conn
 var err error
@@ -56,7 +77,7 @@ func main() {
 	consumergroupnamebase := os.Getenv("CONSUMER_GROUP_NAME")
 	consumergroupname := consumergroupnamebase + "-" + consumerindex
 	fmt.Println(consumergroupname)
-	conn, err = net.Dial("udp", os.Getenv("INFLUX_LINE_IP"))
+	conn, err = net.Dial("tcp", os.Getenv("INFLUX"))
 	if err != nil {
 		fmt.Println("dial error:", err)
 		os.Exit(1)
@@ -64,9 +85,11 @@ func main() {
 
 	config := cluster.NewConfig()
 	config.Consumer.Return.Errors = true
+	config.Consumer.Offsets.Initial = sarama.OffsetNewest
+
 	config.Group.Return.Notifications = true
 	brokers := strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
-	topics := []string{"alamoweblogs"}
+	topics := []string{"istio-access-logs"}
 	consumer, err := cluster.NewConsumer(brokers, consumergroupname, topics, config)
 	if err != nil {
 		fmt.Println(err)
@@ -87,7 +110,7 @@ func main() {
 				if debugoutput {
 					fmt.Fprintf(os.Stdout, "%s/%d/%d\t%s\t%s\n", msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
 				}
-				sock(string(msg.Value[:]))
+				sock(msg.Value, msg.Timestamp)
 				consumer.MarkOffset(msg, "") // mark message as processed
 				consumed++
 			}
@@ -110,120 +133,38 @@ func main() {
 
 }
 
-func sock(logline string) {
-	words := strings.Fields(logline)
-	var fieldmap map[string]string
-	fieldmap = make(map[string]string)
-	for _, element := range words {
-		if strings.Contains(element, "=") {
-			fieldmap[strings.Split(element, "=")[0]] = strings.Split(element, "=")[1]
-		}
+func sock(logline []byte, timestamp time.Time) {
+	var l IstioAccessLog
+	err := json.Unmarshal(logline, &l)
+	if err != nil {
+		fmt.Println(err)
 	}
-	host := fieldmap["hostname"]
-	var fields string
 	var tags string
-        if strings.Contains(logline,"code=H27") {
-           fmt.Println(words)
-           fmt.Println(fieldmap)
-           status := fieldmap["status"]
-           client_closed_time := fieldmap["client_closed_time"]
-           site_domain := fieldmap["site_domain"]
-           fwd := "\"" + strings.Split(fieldmap["fwd"], "%")[0] + "\""
-           tlsversion := "\"" + fieldmap["tlsversion"] + "\""
-           if sendfwdtlsversion {
-                  fields = "fwd=" + fwd + ",tlsversion=" + tlsversion + ","
-           }
-           tags = "fqdn=" + host
-           metricname := "router.client.closed.count"
-           value := "1"
-           send(metricname, tags, fields+"value="+value)
-           metricname = "router.client.closed.ms"
-           value = client_closed_time
-           send(metricname, tags, fields+"value="+value)
-           metricname = "router.status." + status
-           value = "1"
-           send(metricname, tags, fields+"value="+value)
-           metricname = "router.requests.count"
-           value = "1"
-           send(metricname, tags, fields+"value="+value)
-           if site_domain != "" {
-                        host = site_domain
-                        tags = "fqdn=" + host
-                        metricname = "router.client.closed.count"
-                        value = "1"
-                        send(metricname, tags, fields+"value="+value)
-                        metricname = "router.client.closed.ms"
-                        value = client_closed_time
-                        send(metricname, tags, fields+"value="+value)
-                        metricname = "router.status." + status
-                        value = "1"
-                        send(metricname, tags, fields+"value="+value)
-                        metricname = "router.requests.count"
-                        value = "1"
-                        send(metricname, tags, fields+"value="+value)
-           }
-        }
-
-	if !(strings.HasPrefix(host, "alamotest")) && len(words) > 9 && !strings.Contains(strings.Join(words, " "), "4813") && !strings.Contains(logline,"code=H27"){
-		status := fieldmap["status"]
-		service := fieldmap["service"]
-		connect := fieldmap["connect"]
-		total := fieldmap["total"]
-		site_domain := fieldmap["site_domain"]
-		fwd := "\"" + strings.Split(fieldmap["fwd"], "%")[0] + "\""
-		tlsversion := "\"" + fieldmap["tlsversion"] + "\""
-		tags = "fqdn=" + host
-		if sendfwdtlsversion {
-			fields = "fwd=" + fwd + ",tlsversion=" + tlsversion + ","
-		}
-		metricname := "router.service.ms"
-		value := service
-		send(metricname, tags, fields+"value="+value)
-
-		metricname = "router.total.ms"
-		value = total
-		send(metricname, tags, fields+"value="+value)
-
-		metricname = "router.connect.ms"
-		value = connect
-		send(metricname, tags, fields+"value="+value)
-
-		metricname = "router.status." + status
-		value = "1"
-		send(metricname, tags, fields+"value="+value)
-
-		metricname = "router.requests.count"
-		value = "1"
-		send(metricname, tags, fields+"value="+value)
-
-		if site_domain != "" {
-			host = site_domain
-			tags = "fqdn=" + host
-			metricname := "router.service.ms"
-			value := service
-			send(metricname, tags, fields+"value="+value)
-			metricname = "router.total.ms"
-			value = total
-			send(metricname, tags, fields+"value="+value)
-			metricname = "router.connect.ms"
-			value = connect
-			send(metricname, tags, fields+"value="+value)
-			metricname = "router.status." + status
-			value = "1"
-			send(metricname, tags, fields+"value="+value)
-			metricname = "router.requests.count"
-			value = "1"
-			send(metricname, tags, fields+"value="+value)
-		}
-
-	}
+	tags = "fqdn=" + l.Host
+	send("router.service.ms", tags, fixTimeValue(l.Service), timestamp)
+	send("router.total.ms", tags, fixTimeValue(l.Total), timestamp)
+	send("router.status."+strconv.Itoa(l.Status), tags, "1", timestamp)
+	send("router.requests.count", tags, "1", timestamp)
 }
 
-func send(measurement string, tags string, fields string) {
-	if !debugnosend {
-		fmt.Fprintf(conn, measurement+","+tags+" "+fields+"\n")
+func send(measurement string, tags string, value string, timestamp time.Time) {
+	timestamp2 := strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10)
+	put := "put " + measurement + " " + timestamp2 + " " + value + " " + tags + "\n"
+	fmt.Fprintf(conn, put)
+}
+
+func fixTimeValue(value string) (nv string) {
+	var toreturn string
+	if strings.Contains(value, "ms") {
+		toreturn = strings.Replace(value, "ms", "", -1)
+		return toreturn
 	}
-	if debugoutput {
-		fmt.Println(measurement + "," + tags + " " + fields)
+	if strings.Contains(value, "µ") {
+		nounits := strings.Replace(value, "µ", "", -1)
+		converted, _ := strconv.ParseFloat(nounits, 64)
+		multiplied := converted * 0.001
+		toreturn = fmt.Sprintf("%f", multiplied)
+		return toreturn
 	}
+	return toreturn
 }
